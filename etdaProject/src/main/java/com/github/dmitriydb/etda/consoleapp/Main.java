05e478a3119fefeed016acb5287579b206bb7c3d
Dmitriy Drizhilo <drizhiloda@gmail.com>
2021-11-12 10:54:21 +0500

    ETDA-6 ETDA-7 ETDA-8 #close 123

---
+++ etdaProject/src/main/java/com/github/dmitriydb/etda/consoleapp/Main.java
@@ -0,0 +1,13 @@
+package com.github.dmitriydb.etda.consoleapp;
+
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args){
+        Configuration cfg = new Configuration();
+    }
+}
